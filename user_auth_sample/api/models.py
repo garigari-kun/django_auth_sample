70b3c8ce8141c503cd6e29322590081492a1273f
Keisuke Tsukamoto <keisuke.cs@gmail.com>
2016-04-14 18:12:19 +0900

    Added model Task to api app

--- user_auth_sample/api/models.py
+++ user_auth_sample/api/models.py
@@ -1,3 +1,12 @@
 from django.db import models
+from user_auth_sample import settings
 
-# Create your models here.
+class Task(models.Model):
+    user = models.ForeignKey(settings.AUTH_USER_MODEL, related_name = 'tasks')
+    summary = models.CharField(max_length = 120)
+    complete = models.BooleanField(default = False)
+    comment = models.CharField(max_length = 512, blank = True)
+    done_date = models.DateField(null = True, blank = True)
+
+    def __str__(self):
+        return str(self.id) + ': ' + self.summary
